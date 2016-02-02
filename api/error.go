fa67449620811989b534b4f40ffeba4f4f595156
Tanguy Herrmann <dolanorgit@evereska.org>
2016-02-02 01:39:53 +0100

    Major redesign

--- api/error.go
+++ api/error.go
@@ -1,10 +1,15 @@
-package errors
+package api
 
-import (
-	"errors"
-)
+import "errors"
 
 var (
+	// ErrTokenNotFound indicates when the Token is not found in the cookie store.
+	ErrTokenNotFound = errors.New("Token not found in sessions cookiestore")
+	// ErrTokenItemNotFound indicates when an item of the token is not available
+	// It could be that somebody tries to forge fake Token if they somehow got
+	// the server.SymmetricKey.
+	ErrTokenItemNotFound = errors.New("Item not found in token. Possible attack?")
+
 	// ErrUnauthorized indicates when a user is not authorized to access a content
 	ErrUnauthorized = errors.New("Not authorized to access this content.")
 	// ErrDataNotFound indicates when a ressource is not available
--- errors/errors.go
+++
@@ -1,3 +0,0 @@
-// Package errors regroups the errors for all the microservices
-// interaction
-package errors
